c4d76c370bfa7078e6831c6787476a462b03c136
Tomas Harkema <info@tomasharkema.nl>
2014-12-20 12:04:11 +0100

    Strings afkorten

--- index.js
+++ index.js
@@ -5,13 +5,24 @@ var app = express()
 app.get('/:string', function(req, res, next){
 
 	var string = req.params.string;
+	var splitted = string.split(" ");
+	var i = 0;
+	var str = "";
+	splitted.forEach(function(s){
+		if (i%2){
+			str += " "+s;
+		} else {
+			str += "\n"+s;
+		}
+		i++;
+	});
 	gm('hjb.png')
 		.fontSize(80)
 		.font("Impact.ttf")
 		.fill("#fff")
 		.stroke("#000")
 		.strokeWidth(3)
-		.drawText(0, 0, string.toUpperCase(), "North")
+		.drawText(0, 0, str.toUpperCase(), "North")
         .stream(function streamOut (err, stdout, stderr) {
             if (err) return next(err);
             stdout.pipe(res); //pipe to response
